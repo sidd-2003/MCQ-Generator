672a4d53bcd0c9d60a11c053d7c49eb0794cbee2
Siddhant Dnyane <siddhantdnyane2003@gmail.com>
2024-09-03 18:27:50 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages,setup
 setup(
     name='mcqgenrator',
     version='0.0.1',
-    author='iddhant Dnyane',
-    author_email='siddhantdyane2003@gmail.com',
+    author='Siddhant Dnyane',
+    author_email='siddhantdnyane2003@gmail.com',
     install_requires=["groq","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
